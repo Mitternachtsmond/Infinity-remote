069a137cd860b0f299067f8388bcbeaea45cab11
Mitternachtsmond <85838936+Mitternachtsmond@users.noreply.github.com>
2021-06-23 11:46:18 +0530

    Update demo_my.js

--- demo_my.js
+++ demo_my.js
@@ -1,29 +1,3 @@
-// (function(){
-//     var list=document.querySelector('ul');
-//     var items= list.querySelectorAll('button');
-//     var codes={37:-1,38:-5, 39:1,40:5};
-//     for (var i=0;i< items.length;i++){
-//         items[i].count = i ;
-//     }
-
-//     function handlekeys(ev) {
-//         var keycode=ev.keyCode;
-//         if(codes[keycode]) {
-//             // console.log(e)
-//             var t=ev.target;
-//             if(t.count) {
-//                 if(items[t.count + codes[keycode]]) {
-//                     items[t.count + codes[keycode]].focus();
-//                 }
-//             }
-//         }
-//     }
-
-//     list.addEventListener('keyup',handlekeys);
-
-// })();
-
-
 (function(){
     var x= document.querySelector('ul');
     var boxes = x.querySelectorAll('button');
@@ -32,9 +6,9 @@
       40: 3, 
       39: 1,
       37: -1
-    };
-    console.log(x.offsetWidth );
-    console.log( x.firstElementChild.offsetWidth );
+//     };
+//     console.log(x.offsetWidth );
+//     console.log( x.firstElementChild.offsetWidth );
     for (var i = 0; i < boxes.length; i++) {
       boxes[i].index = i;
     }
